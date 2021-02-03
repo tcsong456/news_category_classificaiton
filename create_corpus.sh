a3e8cd3142b1742e41349ed4b26403ed1e2c58e8
Andy Song <congxisong@hotmail.com>
2021-02-03 20:07:44 +0000

    deploy 8

--- create_corpus.sh
+++ create_corpus.sh
@@ -2,6 +2,7 @@ echo 'building corpus'
 if [ ! -f "News_Category_Dataset_v2.json.zip" ];then
   echo "manully download the data from https://www.kaggle.com/rmisra/news-category-dataset"
 fi
+apt-get install unzip
 unzip News_Category_Dataset_v2.json.zip
 python py/build_corpus.py --corpus News_Category_Datset_v2.json > corpus.txt
 wc -l corpus.txt
