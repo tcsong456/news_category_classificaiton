d54ce0a509823a702c6421c5139e0cd009f9cd46
Andy Song <congxisong@hotmail.com>
2021-02-10 15:54:18 +0000

    fix 10

--- azure/batch_score.py
+++ azure/batch_score.py
@@ -73,11 +73,10 @@ def main():
     if args.tag_name is not None and args.tag_value is not None:
         tags = [(args.tag_name,args.tag_value)]
     
-    models = get_model(ws=ws,
-                       model_name=args.model_name,
-                       model_version=args.model_version,
-                       tags=tags)
-    model = models[-1]
+    model = get_model(ws=ws,
+                      model_name=args.model_name,
+                      model_version=args.model_version,
+                      tags=tags)
     model_path = Model.get_model_path(model_name=model.name,
                                       version=model.version,
                                       _workspace=ws)
